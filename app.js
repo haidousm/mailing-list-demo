8b9a195a13a0841e0949d4b8c0160207d7185813
haidousm <moussa@Moussas-MacBook-Pro.local>
2019-09-01 12:45:09 +0300

    Setup Express Server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", (req, res) => {
+
+    res.sendFile(__dirname + "/index.html");
+
+})
+
+app.listen(3000, () => {
+
+    console.log("App is running on port 3000.");
+
+})
